86654a601bdf3fb691da9ae4fce4418a68e41eb0
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-08-19 12:30:44 +0300

    Commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from textwrap import fill
 from tkinter import *
 import customtkinter
 import random
@@ -186,31 +185,6 @@ class App(customtkinter.CTk):
             self.guess()
             self.count += 1
         print(f'It took {self.count} attempts.')
-        
-
-class GradientFrame(Canvas):
-    def __init__(self, parent, color1, color2, **kwargs):
-        Canvas.__init__(self, parent, **kwargs)
-        self._color1 = color1
-        self._color2 = color2
-        self.bind("<Configure>", self._draw_gradient)
-
-    def _draw_gradient(self, event = None):
-        width = self.winfo_width()
-        height = self.winfo_height()
-        limit = width
-        (r1,g1,b1) = self.winfo_rgb(self._color1)
-        (r2,g2,b2) = self.winfo_rgb(self._color2)
-        r_ratio = float(r2-r1) / limit
-        g_ratio = float(g2-g1) / limit
-        b_ratio = float(b2-b1) / limit
-
-        for i in range(limit):
-            nr = int(r1 + (r_ratio * i))
-            ng = int(g1 + (g_ratio * i))
-            nb = int(b1 + (b_ratio * i))
-            color = "#%4.4x%4.4x%4.4x" % (nr,ng,nb)
-            self.create_line(i,0,i,height, tags = ("gradient",), fill = color)
 
 
 if __name__ == "__main__":
