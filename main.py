a7367e082f79e038a92893c313f527ec176a2062
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-09-02 12:14:44 +0300

    Commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from multiprocessing.resource_sharer import stop
+from multiprocessing.sharedctypes import Value
 from tkinter import *
 from threading import Timer
 from time import sleep
 import time
+import tkinter
 import customtkinter
 import random
 import pyglet
@@ -45,11 +48,7 @@ class App(customtkinter.CTk):
         self.unguessed_colors = 0
         self.guessed_colors = 0
         self.guessed_colors_and_positions = 0
-        
-        self.sec_count = 1
-        self.min_count = 0
-        
-        self.show_game()
+        self.show_base()
         
     
     def hover(self, btn, colorfgOnHover, colorfgOnLeave):
@@ -57,7 +56,7 @@ class App(customtkinter.CTk):
         btn.bind("<Leave>", func = lambda i: btn.configure(text_color = colorfgOnLeave))
        
         
-    def show_game(self):
+    def show_base(self):
         self.header_widget = customtkinter.CTkLabel(self, corner_radius = 8, text = '      Mastermind', text_font = self.accent_font_4)
         self.header_widget.place(x = 10, y = 10)
         self.header_widget.configure(
@@ -143,13 +142,14 @@ class App(customtkinter.CTk):
             )
             self.hover(i, self.accent_color8, self.accent_color2)
         
-        self.random_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Random', text_font = self.accent_font_2, command = lambda:self.go())
+        self.random_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Random', text_font = self.accent_font_2, command = lambda:self.play())
         self.random_btn.place(x = 360, y = 80)
-        self.reset_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Reset', text_font = self.accent_font_2)
+        self.reset_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Reset', text_font = self.accent_font_2, command = lambda:self.reset())
         self.reset_btn.place(x = 360, y = 440)
-        self.stats_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Stats', text_font = self.accent_font_2)
+        self.reset_btn.configure(state = tkinter.DISABLED)
+        self.stats_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Stats', text_font = self.accent_font_2, command = lambda:self.stats())
         self.stats_btn.place(x = 360, y = 500)
-        self.quit_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Quit', text_font = self.accent_font_2)
+        self.quit_btn = customtkinter.CTkButton(self, corner_radius = 8, text = 'Quit', text_font = self.accent_font_2, command = lambda:quit())
         self.quit_btn.place(x = 360, y = 560)
         self.quit_btn.configure(
             bg_color = self.accent_color2,
@@ -171,46 +171,135 @@ class App(customtkinter.CTk):
                 height = 50
             )
             self.hover(i, self.accent_color8, self.accent_color2)
-        self.show_dots()
-    
+        self.show_empty_dots()
+
     
     def timer(self):
-        self.after(1000, self.timer)
+        try:
+            if self.reset_btn_pressed == True:
+                return
+        except ValueError:
+            pass
+        
         if self.min_count < 10 and self.sec_count < 10:
             self.timer_widget.configure(text = f'0{self.min_count}:0{self.sec_count}')
+            self.after(1000, self.timer)
 
         elif self.min_count < 10 and self.sec_count >= 10 and self.sec_count != 60:
             self.timer_widget.configure(text = f'0{self.min_count}:{self.sec_count}')
+            self.after(1000, self.timer)
             
         elif self.min_count < 10 and self.sec_count >= 10 and self.sec_count == 60:
             self.min_count += 1
             self.timer_widget.configure(text = f'0{self.min_count}:00')
             self.sec_count = 0
+            self.after(1000, self.timer)
             
         elif self.min_count >= 10 and self.min_count < 60 and self.sec_count >= 10 and self.sec_count == 60:
             self.min_count += 1
             self.timer_widget.configure(text = f'{self.min_count}:00')
             self.sec_count = 0
+            self.after(1000, self.timer)
         
         elif self.min_count >= 10 and self.min_count < 60 and self.sec_count < 10:
             self.timer_widget.configure(text = f'{self.min_count}:0{self.sec_count}')
+            self.after(1000, self.timer)
             
         elif self.min_count >= 10 and self.min_count < 60 and self.sec_count >= 10 and self.sec_count != 60:
             self.timer_widget.configure(text = f'{self.min_count}:{self.sec_count}')
+            self.after(1000, self.timer)
             
         elif self.min_count == 60:
             self.timer_widget.configure(text = 'Time is\nOver')
             return
+        
         self.sec_count += 1
     
     
-    def show_dots(self):
+    def show_empty_dots(self):
+        self.dots_0 = PhotoImage(file = 'assets\\dots_0.png').subsample(8)
+        self.dots_1 = PhotoImage(file = 'assets\\dots_1.png').subsample(8)
+        self.dots_2 = PhotoImage(file = 'assets\\dots_2.png').subsample(8)
+        self.dots_3 = PhotoImage(file = 'assets\\dots_3.png').subsample(8)
+        self.dots_4 = PhotoImage(file = 'assets\\dots_4.png').subsample(8)
+        self.dots_5 = PhotoImage(file = 'assets\\dots_5.png').subsample(8)
+        self.dots_6 = PhotoImage(file = 'assets\\dots_6.png').subsample(8)
+        self.dots_7 = PhotoImage(file = 'assets\\dots_7.png').subsample(8)
+        self.dots_8 = PhotoImage(file = 'assets\\dots_8.png').subsample(8)
+        self.dots_9 = PhotoImage(file = 'assets\\dots_9.png').subsample(8)
+        self.dots_10 = PhotoImage(file = 'assets\\dots_10.png').subsample(8)
+        self.dots_11 = PhotoImage(file = 'assets\\dots_11.png').subsample(8)
+        self.dots_12 = PhotoImage(file = 'assets\\dots_12.png').subsample(8)
+        self.dots_13 = PhotoImage(file = 'assets\\dots_13.png').subsample(8)
+        self.dots_14 = PhotoImage(file = 'assets\\dots_14.png').subsample(8)
+        self.dots_15 = PhotoImage(file = 'assets\\dots_15.png').subsample(8)
+        self.dots_16 = PhotoImage(file = 'assets\\dots_16.png').subsample(8)
+        self.dots_17 = PhotoImage(file = 'assets\\dots_17.png').subsample(8)
+        self.dots_18 = PhotoImage(file = 'assets\\dots_18.png').subsample(8)
+        self.dots_19 = PhotoImage(file = 'assets\\dots_19.png').subsample(8)
+        self.dots_20 = PhotoImage(file = 'assets\\dots_20.png').subsample(8)
+            
+        self.dots_0_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_0_widget.place(x = 23, y = 155, width = 55, height = 40)
+        self.dots_1_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_1_widget.place(x = 23, y = 195, width = 55, height = 40)
+        self.dots_2_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_2_widget.place(x = 23, y = 235, width = 55, height = 40)
+        self.dots_3_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_3_widget.place(x = 23, y = 275, width = 55, height = 40)
+        self.dots_4_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_4_widget.place(x = 23, y = 315, width = 55, height = 40)
+        self.dots_5_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_5_widget.place(x = 23, y = 355, width = 55, height = 40)
+        self.dots_6_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_6_widget.place(x = 23, y = 395, width = 55, height = 40)
+        self.dots_7_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_7_widget.place(x = 23, y = 435, width = 55, height = 40)
+        self.dots_8_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_8_widget.place(x = 23, y = 475, width = 55, height = 40)
+        self.dots_9_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_9_widget.place(x = 23, y = 515, width = 55, height = 40)
+        self.dots_10_widget = customtkinter.CTkLabel(self, image = self.dots_0, fg_color = self.accent_color6, bg_color = self.accent_color6)
+        self.dots_10_widget.place(x = 23, y = 555, width = 55, height = 40)
+    
+    
+    def play(self):
+        self.reset_btn_pressed = False
+        self.sec_count = 1
+        self.min_count = 0
+        self.reset_btn.configure(state = tkinter.NORMAL)
+        for i in (self.random_btn,self.stats_btn,self.quit_btn,self.rules_btn):
+            i.configure(state = tkinter.DISABLED)
+        self.timer()
+        
+        
+        
+        
+    def reset(self):
+        self.reset_btn_pressed = True
+        self.show_base()
+    
+    
+    def stats(self):
         pass
-    
-    
-    def go(self):
-        self.show_dots()
-        self.timer() 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     
     def show_colors(self):
