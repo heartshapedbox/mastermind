eaef8cc3dc7365369213ccca556d292ddde825b8
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2023-01-11 12:33:53 +0200

    Commit

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import random
 
 class Mastermind():
     def __init__(self):
-        self.colors_list = ["yellow","blue","red","orange"]
+        self.colors_list = ["YELLOW","BLUE","RED","ORANGE"]
         self.input_list, self.random_colors_list = [], []
+        
+        
+    def show_header(self):
+        underscore = ""
+        for i in range(1, 13):
+            underscore += "_"
+        print(f"{underscore}COLORS{underscore}\n {' * '.join(str(i) for i in self.colors_list)}\n")
     
     
     def get_random_colors_list(self):
@@ -14,10 +21,14 @@ class Mastermind():
 
 
     def get_user_inputs(self):
-        for i in range(0, len(self.colors_list)):
-            user_input = input(f"Color {i + 1}: ").lower()
-            if user_input in self.colors_list:
+        tries_count = 0
+        while tries_count < len(self.colors_list):
+            user_input = input(f"Enter a color {tries_count + 1}: ").upper()
+            if user_input not in self.colors_list:
+                print("Color is not in the list! Try again!")
+            else:
                 self.input_list.append(user_input)
+                tries_count += 1
         return self.input_list
 
 
@@ -30,8 +41,8 @@ class Mastermind():
                 
 
     def play(self):
-        print(self.colors_list)
-        print(self.get_random_colors_list())
+        self.show_header()
+        self.get_random_colors_list()
         print(self.get_user_inputs())
         self.get_output()
 
